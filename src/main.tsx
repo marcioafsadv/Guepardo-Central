5ca24ec048796c288883485329c096fb0dee52f7
marcioafsadv <marcioafs.adv@gmail.com>
2026-03-23 15:23:28 -0300

    fix(branding): retornar nome oficial e limpar logs de debug

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-console.log('Guepardo Central - V101');
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
